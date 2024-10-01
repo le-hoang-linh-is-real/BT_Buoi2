272e1812d0669a11c217fcc49bc699a12b72bcdd
le-hoang-linh-is-real <3010hoanglinh3010@gmail.com>
2024-10-01 09:16:38 +0700

    thay doi

---
+++ Flyweight/CharacterFactory.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Flyweight
+{
+        public class CharacterFactory
+    {
+        private Dictionary<char, Character> characters = new Dictionary<char, Character>();
+        public Character GetCharacter(char key)
+        {
+            // Uses "lazy initialization"
+            Character character = null;
+            if (characters.ContainsKey(key))
+            {
+                character = characters[key];
+            }
+            else
+            {
+                switch (key)
+                {
+                    case 'A': character = new CharacterA(); break;
+                    case 'B': character = new CharacterB(); break;
+                    //...
+                    case 'Z': character = new CharacterZ(); break;
+                }
+                characters.Add(key, character);
+            }
+            return character;
+        }
+    }
+    /// <summary>
+    /// The 'Flyweight' abstract class
+    /// </summary>
+    public abstract class Character
+    {
+        protected char symbol;
+        protected int width;
+        protected int height;
+        protected int ascent;
+        protected int descent;
+        protected int pointSize;
+        public abstract void Display(int pointSize);
+    }
+
+}
+}
---
+++ Flyweight/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Flyweight.RealWorld
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            // Build a document with text
+            string document = "AAZZBBZB";
+            char[] chars = document.ToCharArray();
+
+            CharacterFactory factory = new CharacterFactory();
+
+            // Extrinsic state
+            int pointSize = 10;
+
+            // For each character use a flyweight object
+            foreach (char c in chars)
+            {
+                pointSize++;
+                Character character = factory.GetCharacter(c);
+                character.Display(pointSize);
+            }
+
+            // Wait for user
+            Console.ReadKey();
+        }
+    }
+}
+
